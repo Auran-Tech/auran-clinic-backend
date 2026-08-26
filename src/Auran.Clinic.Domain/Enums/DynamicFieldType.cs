1e8714d0352c2b1fd9dd1fb17dee0d45e69dd6dd
Ahmed Shahine <102306170+AhmedShahine1@users.noreply.github.com>
2026-08-26 15:52:45 +0300

    refactor: split domain enums into individual files

---
+++ src/Auran.Clinic.Domain/Enums/DynamicFieldType.cs
@@ -0,0 +1,14 @@
+namespace Auran.Clinic.Domain.Enums;
+
+public enum DynamicFieldType
+{
+    Text,
+    LongText,
+    Number,
+    Boolean,
+    Date,
+    Image,
+    File,
+    SingleSelect,
+    MultiSelect
+}
