2f7cbbd4f8185abdb24356a75460eddd39eedca9
Ahmed Shahine <102306170+AhmedShahine1@users.noreply.github.com>
2026-08-27 16:27:23 +0300

    feat: add clinic search request

---
+++ src/Auran.Clinic.Application/Clinics/ClinicSearchRequest.cs
@@ -0,0 +1,9 @@
+namespace Auran.Clinic.Application.Clinics;
+
+public sealed class ClinicSearchRequest
+{
+    public string? Search { get; set; }
+    public bool? IsActive { get; set; }
+    public int Page { get; set; } = 1;
+    public int PageSize { get; set; } = 20;
+}
