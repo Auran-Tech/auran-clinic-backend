3388785b28f093bc2db2c276f502620387b487ad
Ahmed Shahine <102306170+AhmedShahine1@users.noreply.github.com>
2026-08-29 16:02:12 +0300

    fix: use valid empty API contract class body

--- src/Auran.Clinic.Api/Contracts/Clinics/UpdateClinicApiRequest.cs
+++ src/Auran.Clinic.Api/Contracts/Clinics/UpdateClinicApiRequest.cs
@@ -1,3 +1,5 @@
 namespace Auran.Clinic.Api.Contracts.Clinics;
 
-public sealed class UpdateClinicApiRequest : ClinicProfileApiRequest;
+public sealed class UpdateClinicApiRequest : ClinicProfileApiRequest
+{
+}
