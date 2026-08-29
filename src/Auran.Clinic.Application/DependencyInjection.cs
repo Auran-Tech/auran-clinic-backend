0139661fd4806b4a2a928e49101fbd90dffc0632
Ahmed Shahine <102306170+AhmedShahine1@users.noreply.github.com>
2026-08-29 15:58:59 +0300

    refactor: remove application validator registration

--- src/Auran.Clinic.Application/DependencyInjection.cs
+++ src/Auran.Clinic.Application/DependencyInjection.cs
@@ -1,13 +1,8 @@
-using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Auran.Clinic.Application;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApplication(this IServiceCollection services)
-    {
-        services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
-        return services;
-    }
+    public static IServiceCollection AddApplication(this IServiceCollection services) => services;
 }
