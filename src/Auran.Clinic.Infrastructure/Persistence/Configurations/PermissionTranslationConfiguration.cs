11d4dc37799cd6a4ae380f3df6d68a7290ef8a30
Ahmed Shahine <102306170+AhmedShahine1@users.noreply.github.com>
2026-08-31 00:27:29 +0300

    feat: seed Arabic and English permission descriptions

--- src/Auran.Clinic.Infrastructure/Persistence/Configurations/PermissionTranslationConfiguration.cs
+++ src/Auran.Clinic.Infrastructure/Persistence/Configurations/PermissionTranslationConfiguration.cs
@@ -1,3 +1,4 @@
+using Auran.Clinic.Application.Authorization;
 using Auran.Clinic.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
@@ -6,10 +7,32 @@ namespace Auran.Clinic.Infrastructure.Persistence.Configurations;
 
 public sealed class PermissionTranslationConfiguration : IEntityTypeConfiguration<PermissionTranslation>
 {
+    private static readonly DateTime SeedDate = new(2026, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
     public void Configure(EntityTypeBuilder<PermissionTranslation> builder)
     {
         builder.HasIndex(x => new { x.PermissionId, x.LanguageCode }).IsUnique();
         builder.Property(x => x.LanguageCode).HasMaxLength(16).IsRequired();
         builder.Property(x => x.Description).HasMaxLength(500).IsRequired();
+
+        builder.HasData(Permissions.All.SelectMany(definition => new[]
+        {
+            new PermissionTranslation
+            {
+                Id = PermissionSeedIds.Translation(definition.Key, "en"),
+                PermissionId = PermissionSeedIds.Permission(definition.Key),
+                LanguageCode = "en",
+                Description = definition.EnglishDescription,
+                CreatedDate = SeedDate
+            },
+            new PermissionTranslation
+            {
+                Id = PermissionSeedIds.Translation(definition.Key, "ar"),
+                PermissionId = PermissionSeedIds.Permission(definition.Key),
+                LanguageCode = "ar",
+                Description = definition.ArabicDescription,
+                CreatedDate = SeedDate
+            }
+        }));
     }
 }
