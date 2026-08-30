3420ea4b5e871ad625815816ea23e98fef2c99cd
Ahmed Shahine <102306170+AhmedShahine1@users.noreply.github.com>
2026-08-31 00:08:47 +0300

    feat: configure permission translations

---
+++ src/Auran.Clinic.Infrastructure/Persistence/Configurations/PermissionTranslationConfiguration.cs
@@ -0,0 +1,15 @@
+using Auran.Clinic.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Auran.Clinic.Infrastructure.Persistence.Configurations;
+
+public sealed class PermissionTranslationConfiguration : IEntityTypeConfiguration<PermissionTranslation>
+{
+    public void Configure(EntityTypeBuilder<PermissionTranslation> builder)
+    {
+        builder.HasIndex(x => new { x.PermissionId, x.LanguageCode }).IsUnique();
+        builder.Property(x => x.LanguageCode).HasMaxLength(16).IsRequired();
+        builder.Property(x => x.Description).HasMaxLength(500).IsRequired();
+    }
+}
