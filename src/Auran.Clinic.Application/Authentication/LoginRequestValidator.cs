5c1ff6b447eefb9e0a0d71f60aa01e088f356888
Ahmed Shahine <102306170+AhmedShahine1@users.noreply.github.com>
2026-08-31 00:15:17 +0300

    testable validation for login request

---
+++ src/Auran.Clinic.Application/Authentication/LoginRequestValidator.cs
@@ -0,0 +1,18 @@
+using FluentValidation;
+
+namespace Auran.Clinic.Application.Authentication;
+
+public sealed class LoginRequestValidator : AbstractValidator<LoginRequest>
+{
+    public LoginRequestValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress()
+            .MaximumLength(256);
+
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .MaximumLength(256);
+    }
+}
