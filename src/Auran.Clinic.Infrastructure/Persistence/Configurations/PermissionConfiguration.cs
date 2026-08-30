639bdc3f2f0a2d9a3f2ca19dc76e24f899063c94
Ahmed Shahine <102306170+AhmedShahine1@users.noreply.github.com>
2026-08-31 00:27:11 +0300

    feat: seed stable system permission keys

--- src/Auran.Clinic.Infrastructure/Persistence/Configurations/PermissionConfiguration.cs
+++ src/Auran.Clinic.Infrastructure/Persistence/Configurations/PermissionConfiguration.cs
@@ -1,3 +1,4 @@
+using Auran.Clinic.Application.Authorization;
 using Auran.Clinic.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
@@ -6,10 +7,20 @@ namespace Auran.Clinic.Infrastructure.Persistence.Configurations;
 
 public class PermissionConfiguration : IEntityTypeConfiguration<Permission>
 {
+    private static readonly DateTime SeedDate = new(2026, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
     public void Configure(EntityTypeBuilder<Permission> builder)
     {
         builder.HasIndex(x => x.Key).IsUnique();
         builder.Property(x => x.Key).HasMaxLength(160).IsRequired();
         builder.Property(x => x.GroupKey).HasMaxLength(100).IsRequired();
+
+        builder.HasData(Permissions.All.Select(definition => new Permission
+        {
+            Id = PermissionSeedIds.Permission(definition.Key),
+            Key = definition.Key,
+            GroupKey = definition.GroupKey,
+            CreatedDate = SeedDate
+        }));
     }
 }
