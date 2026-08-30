fe006d5cf3cc7775fe8eec9772cfe350319c19d3
Ahmed Shahine <102306170+AhmedShahine1@users.noreply.github.com>
2026-08-31 00:08:35 +0300

    feat: normalize multi-select profile values

---
+++ src/Auran.Clinic.Domain/Entities/PatientProfileValueOption.cs
@@ -0,0 +1,8 @@
+namespace Auran.Clinic.Domain.Entities;
+
+public class PatientProfileValueOption : ClinicEntity
+{
+    public Guid PatientProfileValueId { get; set; }
+
+    public Guid OptionId { get; set; }
+}
