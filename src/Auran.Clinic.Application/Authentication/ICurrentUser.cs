b100c966f8727db6dffe145d70a51ca12f223492
Ahmed Shahine <102306170+AhmedShahine1@users.noreply.github.com>
2026-08-31 00:20:41 +0300

    refactor: remove duplicate current user abstraction

--- src/Auran.Clinic.Application/Authentication/ICurrentUser.cs
+++
@@ -1,9 +0,0 @@
-namespace Auran.Clinic.Application.Authentication;
-
-public interface ICurrentUser
-{
-    bool IsAuthenticated { get; }
-    Guid? UserId { get; }
-    Guid? ClinicId { get; }
-    bool IsSuperUser { get; }
-}
