ea5492ead6f7d75737268e271a472e38f7d4e07a
Ahmed Shahine <102306170+AhmedShahine1@users.noreply.github.com>
2026-09-02 22:01:51 +0300

    foundation: define actor authorization policies

---
+++ src/Auran.Clinic.Application/Authorization/ActorPolicies.cs
@@ -0,0 +1,11 @@
+namespace Auran.Clinic.Application.Authorization;
+
+public static class ActorPolicies
+{
+    public const string Clinic = "Actor.Clinic";
+    public const string Platform = "Actor.Platform";
+
+    public const string ActorTypeClaim = "actor_type";
+    public const string ClinicActor = "clinic";
+    public const string PlatformActor = "platform";
+}
