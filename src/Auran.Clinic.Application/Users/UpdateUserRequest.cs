9af8b9b358fd3f65eb4a2173325d61e1dbaa87a0
Ahmed Shahine <102306170+AhmedShahine1@users.noreply.github.com>
2026-09-11 19:38:20 +0300

    refactor: include user id in update body

--- src/Auran.Clinic.Application/Users/UpdateUserRequest.cs
+++ src/Auran.Clinic.Application/Users/UpdateUserRequest.cs
@@ -2,6 +2,7 @@ namespace Auran.Clinic.Application.Users;
 
 public sealed class UpdateUserRequest
 {
+    public Guid UserId { get; init; }
     public required string FullName { get; init; }
     public required string Email { get; init; }
     public string? Phone { get; init; }
