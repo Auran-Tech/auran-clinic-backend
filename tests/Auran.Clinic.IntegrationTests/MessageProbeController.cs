c9717b1e546a2e4ac70b3af11f8e4e495e0f2d42
Ahmed Shahine <102306170+AhmedShahine1@users.noreply.github.com>
2026-09-11 19:35:33 +0300

    test: add message probe endpoints for all HTTP verbs

---
+++ tests/Auran.Clinic.IntegrationTests/MessageProbeController.cs
@@ -0,0 +1,21 @@
+using Auran.Clinic.Application.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auran.Clinic.IntegrationTests;
+
+[ApiController]
+[Route("_test/message-probe")]
+public sealed class MessageProbeController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<BaseResponse> Get() => Ok(new BaseResponse { Status = true });
+
+    [HttpPost]
+    public ActionResult<BaseResponse> Post() => Ok(new BaseResponse { Status = true });
+
+    [HttpPut]
+    public ActionResult<BaseResponse> Put() => Ok(new BaseResponse { Status = true });
+
+    [HttpDelete]
+    public ActionResult<BaseResponse> Delete() => Ok(new BaseResponse { Status = true });
+}
